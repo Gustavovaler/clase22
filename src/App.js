3963884bb33d64f93ed7982ebb4bab24a2e8f521
gustavovaler <gustavodevaler@ggcode.com.ar>
2021-08-02 20:32:50 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  
+      <div class="jumbotron">
+        <h1 class="display-4">Helle world !!</h1>
+        <p class="lead">
+            This is a simple hero unit, a simple jumbotron-style component <br/>
+            for alling extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <hr class="my-4" />
+        <p>
+            It ussses unity classes for typhography and spacing to space content out <br/>
+            whithin the larger container
+        </p>
+        <a href="" class="btn btn-primary btn-lg"  role="button">Learn More</a>      
     </div>
   );
 }
