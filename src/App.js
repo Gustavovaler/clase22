a67c81d93a1e34a3782ebab5ef21b9a48a2f1f2e
gustavovaler <gustavodevaler@ggcode.com.ar>
2021-08-02 22:36:50 -0300

    segundo ejemplo de estado con axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Boton from './components/Boton'
 import { useState } from 'react'
+import axios  from 'axios'
 
 function App() {
 
@@ -16,20 +17,31 @@ function App() {
     const cambiar_texto = () => {
         setTexto( parrafo )
     }
+    // ----------------------------------
 
+
+
+    const [ personaje , setPersonaje ] = useState('')
+
+    const [id_personaje, setId_personaje ] = useState(3); 
+
+
+    axios.get(`https://swapi.dev/api/people/${id_personaje}`)
+        .then(res => {
+          setPersonaje(res.data.name)})
   return (  
-      <div class="container">
+      <div class="container mt-5">
         <div className="row">
           <div className="col-md-6">
             <div className="card">
                 <div className="card-header">
-                  <h3>Aca va el titulo</h3>
+                  <h3>{ personaje }</h3>
                 </div>
                 <div className="card-body">
-                    <p> { texto } </p>
+                    <p></p>
                 </div>
                 <div className="card-footer">
-                  <button className="btn btn-success btn-block" onClick={cambiar_texto}>Cambiar datos</button>
+                  <button className="btn btn-success btn-block" onClick={() => setId_personaje(4)}>Cambiar personaje</button>
                 </div>
             </div>
 
